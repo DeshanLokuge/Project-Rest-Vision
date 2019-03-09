a1c48abe364006f7e3913f625fe13eb191986a71
thanojl <thanojl@trabeya.com>
2019-03-10 02:50:47 +0530

    v10

--- ui.R
+++ ui.R
@@ -3,11 +3,15 @@ library(shiny)
 library(DT)
 library(shinythemes)
 
+
+city_table <- read.csv("city_table.csv",header = TRUE)
+
+
 shinyUI(
   fluidPage(
     theme = shinytheme("superhero"),
     titlePanel("PROJECT REST-VISION"),
-    #themeSelector(),
+    # themeSelector(),
     navbarPage("REST-VISION", inverse = TRUE,
                
                # Creates a tab panel for Location Search
@@ -16,21 +20,22 @@ shinyUI(
                           sidebarPanel(
                             p("Shows locations of businesses on a map based on your search term."),
                             hr(),
-                            
+                          
                             textInput("search_box", "Type your business here"),
                             
-                            textInput("location_box", "Type your city here"),
+                            ##################################################
+                            selectizeInput("region_box", "Please select region",
+                                           choices=c("United States","Other")),
                             
-                            selectInput("demographic","Demographic of Interest",
-                                        choices = c("Total median age",
-                                                    "Total Median age of Males",
-                                                    "Gross Median Rent",
-                                                    "Mortgage",
-                                                    "Not interested")),
+                            uiOutput("region_output"), #For city selection based on region
+                            uiOutput("region_output2"), #For demographic selection if region = United States
+                            ##################################################
                             
                             actionButton("location_button", label = "", icon = shiny::icon("search"))
+                            
                           ),
                           
+                         
                           # Outputs the map
                           mainPanel(
                             leafletOutput('myMap', height = "800")
@@ -38,25 +43,25 @@ shinyUI(
     )
    ),
 
-   # Creates a tab panel for Business Search
-   tabPanel("Business Search",
-            sidebarLayout(
-              
-              sidebarPanel(
-                p("Shows a table of businesses based on your search terms."),
-                hr(),
-                textInput("search_input", "Type your search here"),
-                textInput("location_input", "Type your location here"),
-                actionButton("search_button", label = "", icon = shiny::icon("search"))
-                
-              ),
-              
-              # outputs the data table of businesses
-              mainPanel(
-                dataTableOutput("businesses")
-              )
-            )
-   )
+               # Creates a tab panel for Business Search
+               tabPanel("Business Search",
+                        sidebarLayout(
+                          
+                          sidebarPanel(
+                            p("Shows a table of businesses based on your search terms."),
+                            hr(),
+                            textInput("search_input", "Type your search here"),
+                            textInput("location_input", "Type your location here"),
+                            actionButton("search_button", label = "", icon = shiny::icon("search"))
+                            
+                          ),
+                          
+                          # outputs the data table of businesses
+                          mainPanel(
+                            dataTableOutput("businesses")
+                          )
+                        )
+               )
    
      )
  )
